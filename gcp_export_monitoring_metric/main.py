e2cf48b80ad41894ab332c585629a94ea77d518f
eladshaabi <elad.eladsh@gmail.com>
2021-03-02 15:42:17 +0200

    Final change

--- gcp_export_monitoring_metric/main.py
+++ gcp_export_monitoring_metric/main.py
@@ -41,11 +41,24 @@ def parse_and_write_as_json_new_line(data, output_file_name):
     points = []
 
     for page in data:
+        metric_name = page.metric
+        resource_name = page.resource
+
         for point in page.points:
+
             dict_point = {
                 'time': point.interval.start_time.strftime('%d/%m/%Y %H:%M:%S'),
+                'metric_type': metric_name.type,
+                'resource_type': resource_name.type,
                 'value': point.value.int64_value
             }
+
+            for key, value in metric_name.labels.items():
+                dict_point[key] = value
+
+            for key, value in resource_name.labels.items():
+                dict_point[key] = value
+
             points.append(dict_point)
 
     with open(f'./{output_file_name}.json', 'w') as out_file:
