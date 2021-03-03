7f9f3ad8a99d9fcfea3150d120245e1daa9357a8
eladshaabi <elad.eladsh@gmail.com>
2021-03-03 17:38:03 +0200

    Final change

--- README.md
+++ README.md
@@ -14,6 +14,10 @@ To run the main file, please use the following arguments:
 
 --hours - The number of hours back to get the metric data.
 
+--mean_series_aligner - Whether to use MEAN aggregation or not.
+
+--alignment_period - Time interval in seconds, that is used to divide the data (use only with mean_series_aligner).
+
 --output_file_name - the JSON output file name that will include the data.
 
 ## Authentication
@@ -29,5 +33,17 @@ python main.py --project=<PROJECT-ID> \
 --hours=2 \
 --filter='metric.type = "pubsub.googleapis.com/topic/send_message_operation_count"' \
 --output_file_name=pubsub_monitorig_data
+```
 
+## Aggregation Example
+
+```
+python main.py --project=<PROJECT-ID> \
+--weeks=5 \
+--days=3 \
+--hours=2 \
+--filter='metric.type = "pubsub.googleapis.com/topic/send_message_operation_count"' \
+--mean_series_aligner=True \
+--alignment_period=120 \
+--output_file_name=pubsub_monitorig_data_2_min_agg
 ```
--- gcp_export_monitoring_metric/main.py
+++ gcp_export_monitoring_metric/main.py
@@ -6,7 +6,7 @@ import os
 import json
 
 
-def get_metric_data(project_id, metric_filter, weeks_ago, days_ago, hours_ago):
+def get_metric_data(project_id, metric_filter, weeks_ago, days_ago, hours_ago, agg, agg_per):
     def get_second_delta(weeks_ago, days_ago, hours_ago):
         return timedelta(weeks=weeks_ago, days=days_ago, hours=hours_ago).total_seconds()
 
@@ -18,18 +18,29 @@ def get_metric_data(project_id, metric_filter, weeks_ago, days_ago, hours_ago):
 
     seconds = int(now)
     nanos = int((now - seconds) * 10 ** 9)
+
     interval = monitoring_v3.TimeInterval(
         {
             "end_time": {"seconds": seconds, "nanos": nanos},
             "start_time": {"seconds": (seconds - int(time_ago)), "nanos": nanos},
         }
     )
+    if agg:
+        aggregation = monitoring_v3.Aggregation(
+            {
+                "alignment_period": {"seconds": agg_per},  # 20 minutes
+                "per_series_aligner": monitoring_v3.Aggregation.Aligner.ALIGN_MEAN,
+            }
+        )
+    else:
+        aggregation = None
 
     results = client.list_time_series(
         request={
             "name": project_name,
             "filter": f'{metric_filter}',
             "interval": interval,
+            "aggregation": aggregation,
             "view": monitoring_v3.ListTimeSeriesRequest.TimeSeriesView.FULL,
         }
     )
@@ -37,9 +48,9 @@ def get_metric_data(project_id, metric_filter, weeks_ago, days_ago, hours_ago):
     return results
 
 
-def parse_and_write_as_json_new_line(data, output_file_name):
-    points = []
+def parse_and_write_as_json_new_line(data, output_file_name, agg):
 
+    points = []
     for page in data:
         metric_name = page.metric
         resource_name = page.resource
@@ -50,8 +61,13 @@ def parse_and_write_as_json_new_line(data, output_file_name):
                 'time': point.interval.start_time.strftime('%d/%m/%Y %H:%M:%S'),
                 'metric_type': metric_name.type,
                 'resource_type': resource_name.type,
-                'value': point.value.int64_value
+               # 'value': point.value.int64_value
             }
+
+            if agg:
+                dict_point['value'] = point.value.double_value
+            else:
+                dict_point['value'] = point.value.int64_value
 
             for key, value in metric_name.labels.items():
                 dict_point[key] = value
@@ -83,11 +99,20 @@ if __name__ == '__main__':
     parser.add_argument("--hours", help="The number of hours back to get the metric data",
                         required=True, type=int)
 
+    parser.add_argument("--mean_series_aligner", help='Whether to use MEAN aggregation or not', type=bool)
+
+    parser.add_argument("--alignment_period", help="Time interval in seconds, that is used to divide the data "
+                                                   "(use only with mean_series_aligner)", type=int)
+
     parser.add_argument("--output_file_name", required=True)
 
     args = parser.parse_args()
 
+    if (args.mean_series_aligner and not args.alignment_period) or\
+            (not args.mean_series_aligner and args.alignment_period):
+        parser.error('To use aggregation please specify both,  mean_series_aligner and alignment_period.')
 
-    raw_metrics_data = get_metric_data(args.project, args.filter, args.weeks, args.days, args.hours)
+    raw_metrics_data = get_metric_data(args.project, args.filter, args.weeks, args.days, args.hours,
+                                       args.mean_series_aligner, args.alignment_period)
 
-    parse_and_write_as_json_new_line(raw_metrics_data, args.output_file_name)
+    parse_and_write_as_json_new_line(raw_metrics_data, args.output_file_name, args.mean_series_aligner)
